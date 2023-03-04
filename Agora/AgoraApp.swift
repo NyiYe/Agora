777c1dea603ef1c41089f1b13e75e36e69a4b71c
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2023-03-04 12:26:23 +0630

    Initial Commit

---
+++ Agora/AgoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgoraApp.swift
+//  Agora
+//
+//  Created by Nyi Ye Han on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AgoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
